4ff67630131f6d4ac1e568830b694e9e617524a8
RogerioNeto07 <joserogeriofonsecauzl@gmail.com>
2025-05-02 23:03:20 -0300

    adicionar controlador

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Despesa
+from .serializers import DespesaSerializer
 
-# Create your views here.
+class DespesaViewSet(viewsets.ModelViewSet):
+    queryset = Despesa.objects.all()
+    serializer_class = DespesaSerializer
